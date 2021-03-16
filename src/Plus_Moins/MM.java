416ab143a671aff54ebdba2a3b24949fd71d2cfc
Adlo777 <loadgoca@gmail.com>
2021-03-16 12:12:03 +0100

    Final commit

---
+++ src/Plus_Moins/MM.java
@@ -0,0 +1,13 @@
+package Plus_Moins;
+
+public class MM {
+	public static void main(String [] args)
+	{
+		int comp=0;
+		while(comp<5)
+		{
+			System.out.println("Le compteur est déjà à " + comp);
+			comp++;
+		}
+	}
+}
--- src/Plus_Moins/Main.java
+++ src/Plus_Moins/Main.java
@@ -1,10 +1,41 @@
 package Plus_Moins;
 
+import java.util.Scanner;
+
 public class Main {
-	
+	private final static Scanner sc = new Scanner(System.in);
 	public static void main(String[] args)
 	{
+		final int MAX = 1000;
+		final int MIN = 1;
+		int nombreAleatoire = (int)(Math.random()*(MAX-MIN))+1;
+		int choix;
+		
+		
+		
+			
+		do
+		{
+			System.out.println("Devinez le nombre ");
+			choix = sc.nextInt();
+			if(choix>nombreAleatoire)
+			{
+				System.out.println("Votre nombre est trop grand");
+			}
+			else if (choix<nombreAleatoire) {
+				System.out.println("Votre nombre est trop petit");
+				
+			}
+			else
+			{
+				System.out.println("Vous avez trouvé le bon nombre");
+			}
+		}
+		while(choix!=nombreAleatoire);
+		
+		
 		
 	}
+		
 
 }
